61ef293c60c429f88ec4709b0dd4f4fd2c647640
Jarmo Erola <jarmo.erola@j-erola.net>
2013-04-30 11:54:31 +0300

    Arguments working

--- SimpleCrypt/SimpleCrypt.cpp
+++ SimpleCrypt/SimpleCrypt.cpp
@@ -1,27 +1,74 @@
 
-
 #include "stdafx.h"
 #include <iostream>
+#include <string>
 
 #include "VigenereLibDLL.h"
 
 using namespace std;
 
-int _tmain(int argc, _TCHAR* argv[]){
+int wmain(int argc, wchar_t *argv[], wchar_t *envp[]){
 
-	cout << "Welcome to SimpleCrypt" << endl;
+	string method, key, algorithm, file, ver;
+	bool help, version;
 
-	nsVigenere::Vigenere vig("123456");
+	ver = "v.0.1";
+	help = false;
+	version = false;
 
-	vig.setKey("98765442");
-	char* txt = "This is some kind of test...";
-	const char* asd = vig.encrypt(txt);
+	if (argc > 1) {
+		for (int i = 1; i < argc; i++) { 
+			char buffer1[4], buffer2[64];
+			int ret;
+			string param, value;
 
-	cout << "Teksti: " << txt << "Kryptattu: '" << asd << "'" << endl;
+			ret = wcstombs( buffer1, argv[i], sizeof(buffer1) );
+			if (ret == 4) buffer1[3]='\0';
+			if (ret) param = buffer1;
 
-	//nsVigenere::Vigenere vig("asd");
+			if (i + 1 != argc) {
+				ret = wcstombs( buffer2, argv[i+1], sizeof(buffer2) );
+				if (ret == 64) buffer2[63]='\0';
+				if (ret) value = buffer2;
+			}
 
-	//vig.encrypt("Tämä on vain jokin testi");
-	
+			if (param.compare("-a") == 0) {
+				algorithm = value;
+			} else if (param.compare("-m") == 0) {
+				method = value;
+			} else if (param.compare("-k") == 0) {
+				key = value;
+			} else if (param.compare("-i") == 0) {
+				file = value;
+			} else if (param.compare("-h") == 0){
+				help = true;
+			} else if (param.compare("-v") == 0){
+				version = true;
+			}
+		}
+	} else {
+		help = true;
+	}
+
+	if(help) {
+		cout << "Usage: simplecrypt [options]..." << endl << endl;
+		cout << "Options:" << endl;
+		cout << "  -h                Display this information" << endl;
+		cout << "  -v                Display version information" << endl;
+		cout << "  -i <file>         Place the input file into <file>" << endl;
+		cout << "  -a <algorithm>    Specify the algorithm into <algorithm>" << endl;
+		cout << "  -m <method>       Specify the method into <method> {encrypt|decrypt}" << endl;
+		cout << "  -k <key>          Set key in <key>" << endl << endl;
+		cout << "For bug reporting, contact:" << endl;
+		cout << "<kim.kostera@student.lamk.fi, jarmo.erola@gmail.com>." << endl;
+		return 0;
+	} else if(version) {
+		cout << "SimpleCrypt " << ver << endl;
+		return 0;
+	} else {
+		cout << "Welcome to SimpleCrypt " << ver << endl;
+
+	}
+
     return 0;
 }
